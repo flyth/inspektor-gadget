flyth/inspektor-gadget

commit 31afe36e6fda02201ee7c90b1662b6a662009cbb

Makefile: Add lint target.
This target uses golangci-lint with the same options than our CI.

Suggested-by: Marga Manterola <mmanterola@microsoft.com>
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

Makefile
@@ -168,6 +168,10 @@ integration-tests: kubectl-gadget
 generate-documentation:
 	go run -tags docs cmd/gen-doc/gen-doc.go -repo $(shell pwd)
 
+lint:
+	# This version number must be kept in sync with CI workflow lint one.
+	docker run --rm -v $(shell pwd):/app -w /app golangci/golangci-lint:v1.46.2 golangci-lint run --fix
+
 # minikube
 LIVENESS_PROBE_INITIAL_DELAY_SECONDS ?= 10
 .PHONY: minikube-install

.github/workflows/inspektor-gadget.yml
@@ -51,6 +51,7 @@ jobs:
     - name: Lint
       uses: golangci/golangci-lint-action@v3.2.0
       with:
+        # This version number must be kept in sync with Makefile lint one.
         version: v1.46.2
         working-directory: /home/runner/work/inspektor-gadget/inspektor-gadget
         # Workaround to display the output:

commit 6c84a7c4b27fb968992470deb542fe1d6439456a

Makefile: Fix lint target by running container as current user.
By doing so, we avoid having the modified file belong to root and then to chown
the file after linting.

Fixes: 31afe36e6fda ("Makefile: Add lint target.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

Makefile
@@ -168,7 +168,7 @@ generate-documentation:
 
 lint:
 	# This version number must be kept in sync with CI workflow lint one.
-	docker run --rm -v $(shell pwd):/app -w /app golangci/golangci-lint:v1.46.2 golangci-lint run --fix
+	docker run --rm --user $(shell id -u):$(shell id -g) -v $(shell pwd):/app -w /app golangci/golangci-lint:v1.46.2 golangci-lint run --fix
 
 # minikube
 LIVENESS_PROBE_INITIAL_DELAY_SECONDS ?= 60